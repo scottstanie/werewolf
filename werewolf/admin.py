a0011d3306761d91109a382a9066f683ca3f136b
scott <scott@cogolabs.com>
2016-04-19 09:38:22 -0400

    better models

--- werewolf/admin.py
+++ werewolf/admin.py
@@ -1,12 +1,26 @@
 from django.contrib import admin
 
-from .models import Game, Character, Switch, UserCharacterRelationship
+from .models import Game, Character, Switch, Matchup
 
 
 class GameAdmin(admin.ModelAdmin):
-    readonly_fields = ('started_date', 'name')
+    readonly_fields = ('started_date',)
+
+
+class MatchupInline(admin.TabularInline):
+    model = Matchup
+    extra = 1
+
+
+class UserAdmin(admin.ModelAdmin):
+    inlines = (MatchupInline,)
+
+
+class CharacterAdmin(admin.ModelAdmin):
+    inlines = (MatchupInline,)
+
 
 admin.site.register(Game, GameAdmin)
-admin.site.register(Character)
+admin.site.register(Character, CharacterAdmin)
 admin.site.register(Switch)
-admin.site.register(UserCharacterRelationship)
+admin.site.register(Matchup)
--- werewolf/models.py
+++ werewolf/models.py
@@ -5,24 +5,36 @@ from django.contrib.auth.models import User
 class Game(models.Model):
     name = models.CharField(max_length=30, unique=True)
     started_date = models.DateTimeField('date started', auto_now_add=True)
-    user_characters = models.ManyToManyField('UserCharacterRelationship')
-
-
-class Character(models.Model):
-
-    name = models.CharField(max_length=200)
-    users = models.ManyToManyField(User, through='UserCharacterRelationship')
 
     def __unicode__(self):
         return self.name
 
 
-class UserCharacterRelationship(models.Model):
+
+class Character(models.Model):
+
+    name = models.CharField(max_length=200)
+    users = models.ManyToManyField(User, through='Matchup')
+
+    def __unicode__(self):
+        return self.name
+
+
+class Matchup(models.Model):
     user = models.ForeignKey(User)
     character = models.ForeignKey(Character)
+    game = models.ForeignKey(Game)
+
+    def __unicode__(self):
+        return '%s as %s in %s' % (self.user, self.character, self.game)
+
 
 
 class Switch(models.Model):
     '''A switch action in a game'''
     victim = models.ForeignKey(User, related_name='victim', default=1)
     initiator = models.ForeignKey(User, related_name='initiator', default=1)
+
+    def __unicode__(self):
+        return '%s switched %s' % (self.initiator, self.victim)
+
